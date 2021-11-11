55ccfaabe36190ffb09135e95a7efe4489101591
mickey-ichi <mickey@react-plus.com>
2021-11-11 11:38:33 +0700

    update

--- lab1/src/App.tsx
+++ lab1/src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Modal } from 'antd';
 
 import {ListUser} from './components/ListUser'
 
@@ -6,10 +7,50 @@ import 'antd/dist/antd.css'
 import './App.css';
 
 function App() {
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+
+    const handleOpenModal = () => {
+        setIsModalVisible(true)
+    }
+
+    const handleAddNew = () => {
+
+    }
+
+    const handleCancel = () => {
+        setIsModalVisible(false)
+    }
+
     return (
         <div className="App">
             <h2>List user</h2>
+            <div className="header-add-user">
+                <button className="ant-btn ant-btn-primary" onClick={handleOpenModal}>
+                    Add New User
+                </button>
+            </div>
             <ListUser />
+            <Modal title="Basic Modal" visible={isModalVisible} footer={null} onCancel={handleCancel}>
+                <div className="field-input-group">
+                    <input placeholder="Avatar" type="text" className="ant-input" />
+                </div>
+                <div className="field-input-group">
+                    <input placeholder="Name" type="text" className="ant-input" />
+                </div>
+                <div className="field-input-group">
+                    <input placeholder="Content" type="text" className="ant-input" />
+                </div>
+                <div className="modal-new-user-footer">
+                    <button className="ant-btn ant-btn-primary" onClick={handleAddNew}>
+                        Save
+                    </button>
+                    <button className="ant-btn" style={{marginLeft: 10}} onClick={handleCancel}>
+                        Cancel
+                    </button>
+                </div>
+            </Modal>
         </div>
     );
 }
